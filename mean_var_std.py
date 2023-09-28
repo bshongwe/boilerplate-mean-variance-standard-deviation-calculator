39eeb05879c50fe6fdbebc983ee6dfb1536eef36
Replit user <>
2023-09-28 17:19:24 +0200

    FCC Mean-V Std-Dev Calc. cert Project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,34 @@
 import numpy as np
 
 def calculate(list):
+  """
+  Calculates the mean, variance, standard deviation, max, min, and sum of the rows, columns, and elements in a 3 x 3 matrix.
 
+  Args:
+    list: A list containing nine numbers.
 
+  Returns:
+    A dictionary containing the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix, in the format specified by the user, with lists instead of NumPy arrays.
 
+  Raises:
+    ValueError: If the list contains less than nine numbers.
+  """
 
-    return calculations
+  # Check if the list contains nine numbers.
+  if len(list) != 9:
+    raise ValueError("List must contain nine numbers.")
+
+  # Convert the list to a 3 x 3 Numpy array.
+  matrix = np.array(list).reshape((3, 3))
+
+  # Calculate the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix.
+  calculations = {
+    "mean": [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix.flatten()).tolist()],
+    "variance": [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix.flatten()).tolist()],
+    "standard deviation": [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix.flatten()).tolist()],
+    "max": [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix.flatten()).tolist()],
+    "min": [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix.flatten()).tolist()],
+    "sum": [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix.flatten()).tolist()]
+  }
+
+  return calculations
